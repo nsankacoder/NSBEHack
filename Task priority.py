db66d00ab1d70efc6b80bde5f16e471c6e86cbef
Anis <76013359+Distiner13@users.noreply.github.com>
2021-02-06 20:22:21 -0500

    Update Task priority.py

--- Task priority.py
+++ Task priority.py
@@ -13,32 +13,60 @@ This expression depends upon a few variables:
 
 
 while True:
+    entarray = []
+
     initial = input('Welcome to Calender, what would you like to add (\'Task\' or \'Event\')?')
     #TASK------------------------------------------------
     if initial.lower() in ['task','t']:
         taskName = input('Please insert task name')
+        class Task:
+            def __init__ (self, timeLeft, taskImp, taskSize):
+                self.timeLeft = timeLeft
+                self.taskImp = taskImp
+                self.taskSize = taskSize
+            def urgency(timeLeft, taskImp, taskSize):
+                urgencyRating = (timeLeft/taskSize)/taskImp
+
+                return urgencyRating
+        
 
         #Days until submission----------------------------------------------------------------------------------------------
         import datetime
         from datetime import date
 
-        def numOfDays(today, subd):
+        def timeLeft(today, subd):
             return (subd-today)
+        try:
+            today = datetime.date.today()
+            subds = input('Please insert the date of the last day in the format (yyyy-mm-dd):')
+            subd = datetime.datetime.strptime(subds,"%Y-%m-%d").date()
+            print((timeLeft(today, subd)), ' left until the submission date of ', taskName)
 
-        today = datetime.date.today()
-        subds = input('Please insert the date of the last day in the format (yyyy-mm-dd):')
-        subd = datetime.datetime.strptime(subds,"%Y-%m-%d").date()
-
-
-
-        print((numOfDays(today, subd)), ' left until the submission date of ', taskName)
-
-        '''
+        except:
+            print('Invalid input.')
+            continue
+        
         #USER RATED IMPORTANCE----------------------------------------------------------------------------------------------
-        taskImp = input ('What would you rate the importance of this task on a scale from 0 to 10?')
+        taskImps = input ('What would you rate the importance (not urgency) of this task on a scale from 0 to 10? (10 being the most important or holds the most value)')
+        taskImp = int(taskImps)
 
-        if taskImp >
-        '''
+        if taskImp>10 or taskImp<0:
+            print('Invalid input.')
+            continue
+        elif taskImp>=0 and taskImp<=3: 
+            print('This task is not important')
+        elif taskImp>=4 and taskImp<=8: 
+            print('This task is relevent')
+        elif taskImp>=8 and taskImp<=9: 
+            print('This task is very important')
+        elif taskImp==10:
+            print('This task is must-do!')
+        
+        #TASK SIZE-----------------------------------------------------------------------------------------------------------
+
+
+        s = Task.urgency(timeLeft,taskSize,taskImp)
+        print(s)
 
     #EVENT------------------------------------------------
     elif initial.lower() in ['event','e']:
